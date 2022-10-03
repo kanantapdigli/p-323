01e47c4dd10b253250e9ec8c7874ac1ed918a51d
Kanan Tapdigli <kanan.tapdigli@procure.az>
2022-10-03 23:51:03 +0400

    initial

---
+++ p323/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace p323.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["test"] = "test";
+            ViewBag.Name = "name";
+            return RedirectToAction("Test");
+        }
+
+        public IActionResult Test()
+        {
+            var test = TempData["test"];
+            var name = ViewBag.Name;
+
+            return Ok();
+        }
+    }
+}
